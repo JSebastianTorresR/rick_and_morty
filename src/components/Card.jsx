7b0f3961e3e0af3a6812dbf571997481709a08a9
JSebastianTorresR <sebtorr2004@gmail.com>
2023-05-26 07:27:19 -0500

    Primer commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+export default function Card(props) {
+   return (
+      <div>
+         <button onClick={props.onClose}>X</button>
+         <h2>{props.name}</h2>
+         <h2>{props.status}</h2>
+         <h2>{props.species}</h2>
+         <h2>{props.gender}</h2>
+         <h2>{props.origin}</h2>
+         <img src={props.image} alt={props.name} />
+      </div>
+   );
+}
+// name: nombre.
+// status: status.
+// species: especie.
+// gender: género.
+// origin: origen (ten en cuenta que el nombre del origen viene dentro de otra propiedad llamada name).
+// image: imagen.
